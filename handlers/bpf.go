c3b7988baae9b0407e1df58875d02638479c0b79
Leonardo Di Donato <leodidonato@gmail.com>
2019-02-09 21:09:55 +0100

    feat: shared informer for BPF resource
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>
    Co-Authored-By: Lorenzo Fontana <lo@linux.com>

---
+++ handlers/bpf.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"github.com/bpftools/bpf-operator/apis/v1alpha1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+func NewBPFSharedInformer(bpfClient dynamic.ResourceInterface, queue workqueue.RateLimitingInterface) cache.SharedInformer {
+	si := cache.NewSharedInformer(
+		&cache.ListWatch{
+			ListFunc: func(options meta_v1.ListOptions) (runtime.Object, error) {
+				res, err := bpfClient.List(options)
+				if err != nil {
+					return nil, err
+				}
+
+				ro := &v1alpha1.BPFList{}
+				ro.Items = make([]*v1alpha1.BPF, len(res.Items))
+				runtime.DefaultUnstructuredConverter.FromUnstructured(res.UnstructuredContent(), ro)
+				return ro, nil
+			},
+			WatchFunc: bpfClient.Watch,
+		},
+		&v1alpha1.BPF{},
+		0,
+	)
+
+	si.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			queue.Add(obj)
+		},
+		UpdateFunc: func(oldobj, newobj interface{}) {
+			queue.Add(newobj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			queue.Add(obj)
+		},
+	})
+	return si
+}
