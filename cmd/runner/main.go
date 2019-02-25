d9e1e3d6fe64b5fe960775e1b75c9903a3ef7460
Leonardo Di Donato <leodidonato@gmail.com>
2019-02-25 23:21:06 +0100

    chore: runner improvements

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -1,34 +1,140 @@
 package main
 
 import (
-	"log"
+	"context"
+	// "encoding/json"
+	"fmt"
+	// "github.com/davecgh/go-spew/spew"
+	"github.com/spf13/viper"
 	"net/http"
+	"os"
+	"time"
 
-	"github.com/leodido/bpf-operator/mapcollector"
+	mapc "github.com/leodido/bpf-operator/mapcollector"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
+var (
+	logg *zap.Logger
+	addr string
+	coll *mapc.MapCollector
+	tout = 2 * time.Second
+)
+
+var root = &cobra.Command{
+	Use:   "runbpf <object file>",
+	Short: "",
+	Long:  ``,
+	Args:  cobra.MinimumNArgs(1),
+	// PreRunE: func(cmd *cobra.Command, args []string) error {
+
+	// },
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// // Setup logging
+		// logConf := []byte(`{
+		// 	"level": "debug",
+		// 	"encoding": "console",
+		// 	"outputPaths": ["stdout"],
+		// 	"errorOutputPaths": ["stderr"],
+		// 	"initialFields": {},
+		// 	"encoderConfig": {
+		// 	  "messageKey": "message",
+		// 	  "levelKey": "level",
+		// 	  "levelEncoder": "lowercase"
+		// 	}
+		// }`)
+		// var cfg zap.Config
+		// if err := json.Unmarshal(logConf, &cfg); err != nil {
+		// 	return err
+		// }
+		// var err error
+		// logg, err = cfg.Build()
+		// if err != nil {
+		// 	return err
+		// }
+		// spew.Dump(logg)
+
+		logg = zap.NewExample()
+
+		// Setup a collector for input BPF program
+		coll = mapc.New(args[0])
+		if err := coll.Setup(); err != nil {
+			return err
+		}
+
+		ctx := context.Background()
+
+		serve(ctx)
+
+		<-ctx.Done()
+		return nil
+	},
+}
+
 func main() {
-	reg := prometheus.NewRegistry()
+	root.PersistentFlags().StringVarP(&addr, "address", "a", ":9387", "address of the server")
+	viper.BindPFlags(root.PersistentFlags())
 
-	c := mapcollector.New("example/dummy.o")
-	if err := c.Setup(); err != nil {
-		log.Fatal(err)
+	// root.Execute()
+	var exitCode int
+	if err := root.Execute(); err != nil {
+		exitCode = 1
+		logg.Fatal("Command returned error", zap.Error(err))
+	}
+	if err := logg.Sync(); err != nil {
+		exitCode = 1
+		fmt.Fprintf(os.Stderr, "Error syncing logs: %v\n", err)
+	}
+	time.Sleep(tout)
+	os.Exit(exitCode)
+}
+
+type registry struct {
+	*prometheus.Registry
+
+	logger *zap.Logger
+}
+
+type logger struct {
+	r *registry
+}
+
+var _ promhttp.Logger = (*logger)(nil)
+
+func (l logger) Println(v ...interface{}) {
+	l.r.logger.Sugar().Info(v...)
+}
+
+func serve(ctx context.Context) {
+	// By using DefaultServeMux profiling endpoints come for free.
+	server := &http.Server{Addr: addr, Handler: http.DefaultServeMux}
+	l := logg.With(zap.String("addr", addr))
+
+	reg := &registry{
+		Registry: prometheus.NewRegistry(),
+		logger:   l.With(zap.String("service", "registry")),
+	}
+	// reg.MustRegister(prometheus.NewGoCollector())
+	reg.MustRegister(coll)
+
+	opt := promhttp.HandlerOpts{
+		ErrorLog: logger{r: reg},
 	}
 
-	log.Println("Starting ...")
-
-	if err := reg.Register(c); err != nil {
-		log.Fatalf("Error during collector registration: %s", err)
-	}
-
-	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
-	http.Handle("/metrics", promHandler)
-
-	listenAddress := ":9387"
-	log.Printf("Listening on %s ...", listenAddress)
-	if err := http.ListenAndServe(listenAddress, nil); err != http.ErrServerClosed {
-		log.Fatalf("Error while starting bpfrunner: %v\n", err)
-	}
+	go func() {
+		http.Handle("/metrics", promhttp.HandlerFor(reg, opt))
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			l.Error("Server error", zap.Error(err))
+		}
+		l.Info("Server closed")
+	}()
+	l.Info("Server started")
+	<-ctx.Done()
+	// Gracefully shutdown server.
+	ctx, cancel := context.WithTimeout(context.Background(), tout)
+	defer cancel()
+	server.Shutdown(ctx)
 }
