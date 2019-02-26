00a5dcc1fc49f6201e86592f3427d858fc26b4ff
Leonardo Di Donato <leodidonato@gmail.com>
2019-02-26 14:53:33 +0100

    docs: example that does packet counting
    Co-Authored-By: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ examples/pkts.c
@@ -0,0 +1,43 @@
+#include "elf/include/bpf.h"
+#include "elf/include/bpf_map.h"
+#include "elf/include/bpf_helpers.h"
+#include "elf/include/trace_printk.h"
+
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/in.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/types.h>
+#include <linux/udp.h>
+#include <stddef.h>
+
+
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
+#endif
+
+struct bpf_map_def SEC("maps/packets") countmap = {
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = sizeof(int),
+    .value_size = sizeof(int),
+    .max_entries = 256,
+};
+
+SEC("socket/prog")
+int socket_prog(struct __sk_buff *skb) {
+  int proto = load_byte(skb, ETH_HLEN + offsetof(struct iphdr, protocol));
+  int one = 1;
+  int *el = bpf_map_lookup_elem(&countmap, &proto);
+  if (el) {
+    (*el)++;
+  } else {
+    el = &one;
+  }
+  bpf_map_update_elem(&countmap, &proto, el, BPF_ANY);
+  return 0;
+}
+
+char _license[] SEC("license") = "GPL";
+
+unsigned int _version SEC("version") = 0xFFFFFFFE; // this tells to the ELF loader to set the current running kernel version
